08b4f60960eefe3bb616682584df22cc36eb49e5
MShip <meshippee@gmail.com>
2021-05-09 22:33:54 -0600

    adding comments to prob3

--- prob3.py
+++ prob3.py
@@ -10,13 +10,12 @@ large_prime = 600851475143
 target = int(large_prime**(1/2))
 temp = large_prime
 old_temp = 0
-for i in range(2, target-1):
-    print(i)
-    while temp%i == 0:
-        old_temp = temp
+for i in range(2, target-1): #iterate through numbers and the sqrt of the target
+    while temp%i == 0: # if we divide by everything, whatever's left will be prime
+        old_temp = temp # this is just a check to see if we are done. If nothing changes between iterations, we need to break
         if temp != i:
             temp = temp/i
         if temp == old_temp:
             break
 
-print(temp)
+print(temp) #return the value
